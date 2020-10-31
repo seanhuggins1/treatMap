3cb04cb97e23986a0fedfebb2985586953564699
Sean <seanhugg@gmail.com>
2020-10-30 22:12:07 -0400

    Added address search
    used mapbox geocoder to perform address search

--- map.js
+++ map.js
@@ -1,16 +1,16 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1Z2dpbnMiLCJhIjoiY2tnd24xbW5jMGJsczJxbG5yMGEzazQ5aiJ9.FxooIHh6YNpvjBtY7Im8PQ';
 
-async function getAddressLngLat(address){
+async function getAddressLngLat(address) {
       //setup a URL object to the mapbox geocoding api with our address
       let url = new URL(`/geocoding/v5/mapbox.places/${address}.json`, 'https://api.mapbox.com');
       url.searchParams.append("access_token", mapboxgl.accessToken);
-      
+
       //use fetch to call the mapbox API
       let response = await fetch(url);
       let json = await response.json();
 
-      
 
+      console.log(JSON.stringify(json, null, 2));
       return json.features[0].center;
 }
 
@@ -38,11 +38,11 @@ function initMap(position) {
       map = new mapboxgl.Map({
             container: 'map', // container id
             style: 'mapbox://styles/shuggins/ckgwnjfgn0yl119s4bob2uyx9', // style URL
-            center: [position.coords.longitude, position.coords.latitude], // starting position [lng, lat]
-            zoom: 17 // starting zoom
+            center: [0, 0], // starting position [lng, lat]
+            zoom: 2 // starting zoom
       });
 
-      
+
       map.on("load", function () {
 
 
@@ -57,7 +57,7 @@ function initMap(position) {
       });
 }
 
-getLocation();
+//getLocation();
 
 
 
