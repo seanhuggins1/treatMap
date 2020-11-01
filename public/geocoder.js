7b12d2a7bdf9817342598229aa15c8de9326155e
Sean <seanhugg@gmail.com>
2020-11-01 02:22:02 -0500

    some more ui tweaks

--- public/geocoder.js
+++ public/geocoder.js
@@ -1,13 +1,31 @@
-import {treatData, nextQuestion} from './main.js'
+import {treatData, goToQuestion} from './main.js'
 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1Z2dpbnMiLCJhIjoiY2tnMWliZDB6MnVreDJ5czM1Yzg4b29jOCJ9.-kx3vPkxCh-EbUKbY9Jc0Q';
-var geocoder = new MapboxGeocoder({
+
+//geocoder for choosing treat
+var geocoder1 = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      limit: 3,
+});
+geocoder1.addTo('#geocoder-1');
+geocoder1.on('result', function(result) {
+      treatData.center = result.result.center;
+      goToQuestion(2);
+});
+
+
+//geocoder for choosing treat giving location
+var geocoder2 = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       types: 'address',
       limit: 3,
 });
-geocoder.addTo('#geocoder');
+geocoder2.addTo('#geocoder-2');
+geocoder2.on('result', function(result) {
+      treatData.center = result.result.center;
+      goToQuestion(4);
+});
 
 
 export async function getLocation(coordinates){
@@ -22,7 +40,3 @@ export async function getLocation(coordinates){
 }
 
 
-geocoder.on('result', function(result) {
-      treatData.center = result.result.center;
-      nextQuestion();
-});
